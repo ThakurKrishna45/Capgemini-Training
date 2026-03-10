70488124d1e90ef54653ec33e7932830242c6885
Krishna Pratap Singh <215252541+ThakurKrishna45@users.noreply.github.com>
2026-03-10 16:11:17 +0530

    Update Main.java

--- TrainingAdvance/src/ExceptionHandling/Main.java
+++ TrainingAdvance/src/ExceptionHandling/Main.java
@@ -2,7 +2,6 @@ package ExceptionHandling;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Exception");
         try{
             ElgibilityCheck.check(6.3);
         }catch (Exception e){
